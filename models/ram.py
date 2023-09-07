985a90b8121dc11884e82a0848feb76a09276495
zrainj <1006537395@qq.com>
2023-09-07 16:34:25 +0800

    ram done

--- models/ram.py
+++ models/ram.py
@@ -4,18 +4,16 @@
 # Copyright (C) 2018. All Rights Reserved.
 
 from layers.dynamic_rnn import DynamicLSTM
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
 import mindspore
+import numpy as np
 
 class RAM(mindspore.nn.Cell):
     def locationed_memory(self, memory, memory_len, left_len, aspect_len):
         batch_size = memory.shape[0]
         seq_len = memory.shape[1]
-        memory_len = memory_len.cpu().numpy()
-        left_len = left_len.cpu().numpy()
-        aspect_len = aspect_len.cpu().numpy()
+        memory_len = memory_len.numpy()
+        left_len = left_len.numpy()
+        aspect_len = aspect_len.numpy()
         weight = [[] for i in range(batch_size)]
         u = [[] for i in range(batch_size)]
         for i in range(batch_size):
@@ -34,23 +32,27 @@ class RAM(mindspore.nn.Cell):
         u = mindspore.tensor(u, dtype=memory.dtype).unsqueeze(2)
         weight = mindspore.tensor(weight).unsqueeze(2)
         v = memory*weight
-        memory = mindspore.ops.cat([v, u], dim=2)
+        memory = mindspore.ops.cat([v, u], axis=2)
         return memory
 
     def __init__(self, embedding_matrix, opt):
         super(RAM, self).__init__()
         self.opt = opt
-        self.embed = nn.Embedding.from_pretrained(mindspore.tensor(embedding_matrix, dtype=ms.float32))
+        rows, cols = embedding_matrix.shape
+        self.embed = mindspore.nn.Embedding(rows, cols, embedding_table=mindspore.tensor(embedding_matrix, dtype=mindspore.float32))
         self.bi_lstm_context = DynamicLSTM(opt.embed_dim, opt.hidden_dim, num_layers=1, batch_first=True, bidirectional=True)
         self.att_linear = mindspore.nn.Dense(opt.hidden_dim*2 + 1 + opt.embed_dim*2, 1)
-        self.gru_cell = nn.GRUCell(opt.hidden_dim*2 + 1, opt.embed_dim)
+        self.gru_cell = mindspore.nn.GRUCell(opt.hidden_dim*2 + 1, opt.embed_dim)
         self.dense = mindspore.nn.Dense(opt.embed_dim, opt.polarities_dim)
 
     def construct(self, inputs):
         text_raw_indices, aspect_indices, text_left_indices = inputs[0], inputs[1], inputs[2]
-        left_len = mindspore.ops.sum(text_left_indices != 0, dim=-1)
-        memory_len = mindspore.ops.sum(text_raw_indices != 0, dim=-1)
-        aspect_len = mindspore.ops.sum(aspect_indices != 0, dim=-1)
+        t_1 = mindspore.tensor(np.array(text_left_indices) != 0, mindspore.int32)
+        left_len = mindspore.ops.sum(t_1, dim=-1)
+        t_2 = mindspore.tensor(np.array(text_raw_indices) != 0, mindspore.int32)
+        memory_len = mindspore.ops.sum(t_2, dim=-1)
+        t_3 = mindspore.tensor(np.array(aspect_indices) != 0, mindspore.int32)
+        aspect_len = mindspore.ops.sum(t_3, dim=-1)
         nonzeros_aspect = aspect_len.float()
 
         memory = self.embed(text_raw_indices)
@@ -60,17 +62,17 @@ class RAM(mindspore.nn.Cell):
         aspect = self.embed(aspect_indices)
         aspect = mindspore.ops.sum(aspect, dim=1)
         aspect = mindspore.ops.div(aspect, nonzeros_aspect.unsqueeze(-1))
-        et = torch.zeros_like(aspect)
+        et = mindspore.ops.zeros_like(aspect)
 
-        batch_size = memory.size(0)
-        seq_len = memory.size(1)
+        batch_size = memory.shape[0]
+        seq_len = memory.shape[1]
         for _ in range(self.opt.hops):
             g = self.att_linear(mindspore.ops.cat([memory, 
-                mindspore.ops.zeros(batch_size, seq_len, self.opt.embed_dim) + et.unsqueeze(1), 
-                mindspore.ops.zeros(batch_size, seq_len, self.opt.embed_dim) + aspect.unsqueeze(1)], 
-                dim=-1))
+                mindspore.ops.zeros((batch_size, seq_len, self.opt.embed_dim)) + et.unsqueeze(1), 
+                mindspore.ops.zeros((batch_size, seq_len, self.opt.embed_dim)) + aspect.unsqueeze(1)], 
+                axis=-1))
             alpha = mindspore.ops.softmax(g, axis=1)
-            i = torch.bmm(alpha.transpose(1, 2), memory).squeeze(1)  
+            i = mindspore.ops.bmm(mindspore.ops.swapaxes(alpha, 1, 2), memory).squeeze(1)  
             et = self.gru_cell(i, et)
         out = self.dense(et)
         return out
